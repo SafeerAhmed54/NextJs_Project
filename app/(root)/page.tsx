efcd6996a96c37d8039a7c81487e7ee51a1a1654
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-05-28 01:39:37 +0500

    Added Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <h1 className='text-2xl font-light'>Home</h1>
+    <h1 className='heading'>Home</h1>
     <h1 className='text-2xl'>Start Working on it</h1>
     </>
   )
