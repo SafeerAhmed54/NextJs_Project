ce6fd4bb0ea7471901e5d5688ab546dedcc1c1c2
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-06-06 18:48:58 +0500

    Added Sanity Fetch

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import SearchForm from '../../components/SearchForm'
-import StartupCard, { StartupTypeCard } from '../../components/StartupCard'
-import { client } from '@/sanity/lib/client'
-import { STARTUP_QUERY } from '@/lib/queries'
+import React from "react";
+import SearchForm from "../../components/SearchForm";
+import StartupCard, { StartupTypeCard } from "../../components/StartupCard";
+//import { client } from '@/sanity/lib/client'
+import { STARTUP_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
-const page = async ({searchParams}: {searchParams: Promise<{query?: string}>}) => {
-  const query = (await searchParams).query
-  
-  const posts = await client.fetch(STARTUP_QUERY);
+const page = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const query = (await searchParams).query;
+
+  //const posts = await client.fetch(STARTUP_QUERY);
+
+  const { data: posts } = await sanityFetch({ query: STARTUP_QUERY });
 
   console.log(JSON.stringify(posts), null, 2);
 
   // Uncomment the following lines to use static data for testing
   // Initial posts data can be used for testing purposes
 
-  // const posts = [{ 
+  // const posts = [{
   //   _createdAt : new Date(),
   //   views: 55,
   //   author: {_id: 1 , name: 'Safeer Ahmad Rana'},
@@ -26,32 +33,37 @@ const page = async ({searchParams}: {searchParams: Promise<{query?: string}>}) =
 
   return (
     <>
-    <section className='pink_container'>
-      <h1 className='heading'>Pitch Your Startup,<br /> Connect with Entrepreneurs</h1>
-      <p className='sub-heading !max-w-3xl'>
-        Join a community of startup founders and investors to get feedback,
-        connect with potential customers, and grow your business.
-      </p>
-      <SearchForm query={query}/>
-    </section>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup,
+          <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Join a community of startup founders and investors to get feedback,
+          connect with potential customers, and grow your business.
+        </p>
+        <SearchForm query={query} />
+      </section>
 
-    <section className='section_container'>
-      <p className='text-30-semibold'>
-      {query ? `Search results for "${query}"` : "All Startups" }
-      </p>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
 
-      <ul className='mt-7 card_grid'>
-        {posts?.length > 0 ? (
-        posts.map((post: StartupTypeCard)=>(
-          <StartupCard key={post?._id} post={post}/>
-        ))
-        ): 
-        <p className='no-results'>No Startup Found</p>}
-      </ul>
-    </section>
-    
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startup Found</p>
+          )}
+        </ul>
+      </section>
+
+      <SanityLive />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
