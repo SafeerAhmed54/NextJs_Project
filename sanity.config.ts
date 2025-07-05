44d1e4061b1bc26593e291d77618bbc313b4b268
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-07-05 15:54:56 +0500

    Update

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,3 @@
-"use client";
-
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
@@ -8,23 +6,21 @@ import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schemaTypes";
 import { structure } from "./sanity/structure";
 import { markdownSchema } from "sanity-plugin-markdown";
 
-export default defineConfig({
+const config = defineConfig({
   basePath: "/studio",
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
     structureTool({ structure }),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
     markdownSchema(),
   ],
 });
+
+export default config;
