2d58acf857ee5562940768415565f98ceac34603
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-07-07 00:55:20 +0500

    Added Image on Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { auth, signOut, signIn } from "@/auth";
 import { BadgePlus, LogOut } from "lucide-react";
-//import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = async () => {
   const session = await auth();
@@ -36,13 +36,13 @@ const Navbar = async () => {
               </form>
 
               <Link href={`/user/${session?.id}`}>
-                {/* <Avatar className="size-10">
+                <Avatar className="size-10">
                   <AvatarImage
                     src={session?.user?.image || ""}
                     alt={session?.user?.name || ""}
                   />
                   <AvatarFallback>AV</AvatarFallback>
-                </Avatar> */}
+                </Avatar>
               </Link>
             </>
           ) : (
